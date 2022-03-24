d71569f1f9b678dde9bf0aad44bf7af17dc6d20c
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-24 13:01:03 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Product from "./Product";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Product />
+    </div>
+  );
 }
 
 export default App;
