a5d995ea58a1c6cbb2c22c24936f02a285555993
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-24 13:14:27 +0530

    products

--- src/Product.jsx
+++ src/Product.jsx
@@ -14,6 +14,47 @@ import {
   Box,
   List,
 } from "@material-ui/core";
+
+const styles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  linespace: {
+    padding: theme.spacing(1, 1),
+  },
+  rootCard: {
+    width: 250,
+  },
+  bulletCard: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  titleCard: {
+    fontSize: 20,
+  },
+  posCard: {
+    marginBottom: 12,
+  },
+  footeBox: {
+    width: "100%",
+    backgroundColor: "#3f51b5",
+    position: "fixed",
+    bottom: "2px",
+    marginLeft: "0px",
+    marginBottom: "-2px",
+    height: "130px",
+  },
+  footerList: {
+    textAlign: "center",
+  },
+}));
 const Product = () => {
   return <div></div>;
 };
